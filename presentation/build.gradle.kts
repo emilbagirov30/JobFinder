dffd2a8678f0cd8f259bc6479396dee6a04937e9
emilbagirov30 <emil_bgr@mail.ru>
2024-08-22 21:23:37 +0300

    added dependencies for the "presentation" module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -36,7 +36,9 @@ android {
 }
 
 dependencies {
-
+    implementation (project(":core"))
+    implementation (project(":domain"))
+    implementation (project(":di"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
