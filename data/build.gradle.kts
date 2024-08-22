a9973197cfdea2ed74cd69fcd14392b11c4b74aa
emilbagirov30 <emil_bgr@mail.ru>
2024-08-22 21:17:16 +0300

    added dependencies for the "data" module

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation (project(":core"))
+    implementation (project(":domain"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
